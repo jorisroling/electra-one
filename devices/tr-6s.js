e195e509f23dab96dce5101332e901a64e840480
Joris Röling <joris@bonboa.com>
2021-06-09 23:07:29 +0200

    TR-6S

---
+++ devices/tr-6s.js
@@ -0,0 +1,173 @@
+module.exports = {
+  manufacturer: 'Roland',
+  model: 'TR-6S',
+  version: 'v1.01',
+  notes: {
+    instrument: {
+      bd: {
+        note: 36,
+        alternative: {
+          note: 35,
+        },
+      },
+      sd: {
+        note: 38,
+        alternative: {
+          note: 40,
+        },
+      },
+      lt: {
+        note: 43,
+        alternative: {
+          note: 41,
+        },
+      },
+      hc: {
+        note: 39,
+        alternative: {
+          note: 54,
+        },
+      },
+      ch: {
+        note: 42,
+        alternative: {
+          note: 44,
+        },
+      },
+      oh: {
+        note: 46,
+        alternative: {
+          note: 55,
+        },
+      },
+    },
+  },
+  parameters: {
+    beat: {
+      cc: 2,
+    },
+    suffle: {
+      cc: 9,
+    },
+    fill: {
+      in: {
+        trig: {
+          cc: 70,
+        },
+      },
+    },
+    delay: {
+      level: {
+        cc: 16,
+      },
+      time: {
+        cc: 17,
+      },
+      feedback: {
+        cc: 18,
+      },
+    },
+    reverb: {
+      level: {
+        cc: 91,
+      },
+    },
+    master: {
+      fx: {
+        active: {
+          cc: 15,
+        },
+        ctrl: {
+          cc: 19,
+        },
+      },
+    },
+    instrument: {
+      bd: {
+        tune: {
+          cc: 20,
+        },
+        decay: {
+          cc: 23,
+        },
+        ctrl: {
+          cc: 96,
+        },
+        level: {
+          cc: 24,
+        },
+      },
+      sd: {
+        tune: {
+          cc: 25,
+        },
+        decay: {
+          cc: 28,
+        },
+        ctrl: {
+          cc: 97,
+        },
+        level: {
+          cc: 29,
+        },
+      },
+      lt: {
+        tune: {
+          cc: 46,
+        },
+        decay: {
+          cc: 47,
+        },
+        ctrl: {
+          cc: 102,
+        },
+        level: {
+          cc: 48,
+        },
+      },
+      hc: {
+        tune: {
+          cc: 58,
+        },
+        decay: {
+          cc: 59,
+        },
+        ctrl: {
+          cc: 106,
+        },
+        level: {
+          cc: 60,
+        },
+      },
+      ch: {
+        tune: {
+          cc: 61,
+        },
+        decay: {
+          cc: 62,
+        },
+        ctrl: {
+          cc: 107,
+        },
+        level: {
+          cc: 63,
+        },
+      },
+      oh: {
+        tune: {
+          cc: 80,
+        },
+        decay: {
+          cc: 81,
+        },
+        ctrl: {
+          cc: 108,
+        },
+        level: {
+          cc: 82,
+        },
+      },
+    },
+  },
+}
+
