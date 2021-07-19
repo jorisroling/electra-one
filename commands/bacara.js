17e88b4c4d4784657169d44051442f38ccc36d73
Joris Röling <joris@bonboa.com>
2021-07-19 21:34:07 +0200

    Bacara (acid.v2 + router) mode

---
+++ commands/bacara.js
@@ -0,0 +1,11 @@
+function bacara(name, sub, options) {
+  require('./acid.v2').handler('acid.v2',sub,options)
+  require('./router').handler('router',sub,options)
+}
+
+module.exports = {
+  name: 'bacara',
+  description: 'To it all',
+  handler: bacara,
+  aliases:[]
+}
